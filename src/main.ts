5758619bf6e41b6fe6d2d5095cd83f77d408b839
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2021-12-11 18:59:12 -0300

    Added swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,33 @@
+import { INestApplication } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
+function configureSwagger(app: INestApplication, configService: ConfigService) {
+  const config = new DocumentBuilder()
+    .setTitle(configService.get('APP_NAME'))
+    .setDescription(configService.get('APP_DESCRIPTION'))
+    .setVersion(configService.get('APP_VERSION'))
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup(
+    configService.get('APP_DOCS_PATH') || 'docs',
+    app,
+    document,
+  );
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('app');
+  app.setGlobalPrefix('api');
 
-  const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+  const configService = new ConfigService();
+
+  configureSwagger(app, configService);
 
   await app.listen(3000);
 }
