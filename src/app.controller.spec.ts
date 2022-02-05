c4f5b336e9e09ecb6696a532aa662fe627658d03
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2022-02-05 15:27:58 -0300

    Fixed unit test on app controller

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -4,19 +4,38 @@ import { AppService } from './app.service';
 
 describe('AppController', () => {
   let appController: AppController;
+  let appService: AppService;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        {
+          provide: AppService,
+          useValue: {
+            getStatus: jest.fn().mockReturnValue({
+              status: 'Ok',
+            })
+          }
+        }
+      ],
     }).compile();
 
     appController = app.get<AppController>(AppController);
+    appService = app.get<AppService>(AppService);
+  });
+
+  it('should be defined', () => {
+    expect(appController).toBeDefined();
+    expect(appService).toBeDefined();
   });
 
   describe('root', () => {
     it('should return status "Ok"', () => {
-      expect(appController.getStatus().status).toBe('Ok');
+      expect(appController.getStatus()).toEqual({
+        status: 'Ok',
+      });
+      expect(appService.getStatus).toHaveBeenCalledTimes(1);
     });
   });
 });
