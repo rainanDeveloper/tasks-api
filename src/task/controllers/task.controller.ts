a4192b80b4a7e8101a408379a3991a6f0560be81
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2022-02-25 19:37:13 -0300

    feat(#14): added jwt auth guard to validate protected endpoints
    added jwt auth guard to validate protected endpoints

--- src/task/controllers/task.controller.ts
+++ src/task/controllers/task.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
+import { AuthUserJwtGuard } from '../../user/guards/auth-user-jwt.guard';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
 @Controller('task')
+@ApiTags('Tasks')
+@ApiBearerAuth()
+@UseGuards(AuthUserJwtGuard)
 export class TaskController {}
